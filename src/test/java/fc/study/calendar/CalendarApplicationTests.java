0156345e85e30f5b8bd350b90fe7b722c1d451c3
김우진 <dnwls488@naver.com>
2022-09-28 00:15:59 +0900

    Initial commit

---
+++ src/test/java/fc/study/calendar/CalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package fc.study.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalendarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
